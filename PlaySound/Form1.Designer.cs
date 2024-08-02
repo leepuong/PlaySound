9acadd7ddb4c0c0854e57e14d7cabad56fc80b67
leepuong <hhyfuihggtuucg@gmail.com>
2024-08-03 03:18:35 +0700

    add project playsound

---
+++ PlaySound/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace PlaySound
+{
+    partial class frmPlaySound
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPlaySound));
+            this.btnFF = new System.Windows.Forms.Button();
+            this.btnAngHiu = new System.Windows.Forms.Button();
+            this.lblStop = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnFF
+            // 
+            this.btnFF.BackColor = System.Drawing.Color.Red;
+            this.btnFF.Location = new System.Drawing.Point(183, 337);
+            this.btnFF.Name = "btnFF";
+            this.btnFF.Size = new System.Drawing.Size(75, 23);
+            this.btnFF.TabIndex = 0;
+            this.btnFF.Text = "FreeFire";
+            this.btnFF.UseVisualStyleBackColor = false;
+            this.btnFF.Click += new System.EventHandler(this.btnFF_Click);
+            // 
+            // btnAngHiu
+            // 
+            this.btnAngHiu.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnAngHiu.Location = new System.Drawing.Point(538, 337);
+            this.btnAngHiu.Name = "btnAngHiu";
+            this.btnAngHiu.Size = new System.Drawing.Size(75, 23);
+            this.btnAngHiu.TabIndex = 0;
+            this.btnAngHiu.Text = "ĂngHiu";
+            this.btnAngHiu.UseVisualStyleBackColor = false;
+            this.btnAngHiu.Click += new System.EventHandler(this.btnAngHiu_Click);
+            // 
+            // lblStop
+            // 
+            this.lblStop.AutoSize = true;
+            this.lblStop.BackColor = System.Drawing.Color.Transparent;
+            this.lblStop.ForeColor = System.Drawing.Color.Transparent;
+            this.lblStop.Location = new System.Drawing.Point(373, 20);
+            this.lblStop.Name = "lblStop";
+            this.lblStop.Size = new System.Drawing.Size(35, 16);
+            this.lblStop.TabIndex = 1;
+            this.lblStop.Text = "Stop";
+            this.lblStop.Click += new System.EventHandler(this.lblStop_Click);
+            // 
+            // frmPlaySound
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackgroundImage = global::PlaySound.Properties.Resources.morpheus1_1590897312;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(793, 451);
+            this.Controls.Add(this.lblStop);
+            this.Controls.Add(this.btnAngHiu);
+            this.Controls.Add(this.btnFF);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmPlaySound";
+            this.Text = "Your choice";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFF;
+        private System.Windows.Forms.Button btnAngHiu;
+        private System.Windows.Forms.Label lblStop;
+    }
+}
+
---
+++ PlaySound/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlaySound
+{
+    public partial class frmPlaySound : Form
+    {
+        private SoundPlayer soundPlayerFF;
+        private SoundPlayer soundPlayerAH;
+        private SoundPlayer soundPlayer;
+        public frmPlaySound()
+        {
+            InitializeComponent();
+
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            // Đi lên hai cấp để đến được thư mục gốc của dự án
+            string projectDirectory = System.IO.Path.GetFullPath(System.IO.Path.Combine(baseDirectory, @"..\..\"));
+
+            // Kết hợp đường dẫn thư mục gốc với thư mục Resources và tên file âm thanh
+            string soundFileFF = System.IO.Path.Combine(projectDirectory, "Resources", "freefiresound.wav");
+            string soundFileAH = System.IO.Path.Combine(projectDirectory, "Resources", "anghieusound.wav");
+
+
+
+            soundPlayerFF = new SoundPlayer(soundFileFF);
+
+
+            // Gán sự kiện Click cho btnPlaySound
+            btnFF.Click += btnFF_Click;
+
+
+            soundPlayerAH = new SoundPlayer(soundFileAH);
+
+            // Gán sự kiện Click cho btnPlaySound
+            btnAngHiu.Click += btnAngHiu_Click;
+
+
+            lblStop.Click += lblStop_Click;
+        }
+
+        private void btnFF_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                soundPlayerFF.Play();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error playing sound: " + ex.Message);
+            }
+
+        }
+
+        private void btnAngHiu_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                soundPlayerAH.Play();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error playing sound: " + ex.Message);
+            }
+        }
+
+        private void lblStop_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                soundPlayerAH.Stop();
+                soundPlayerFF.Stop();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error stopping sound: " + ex.Message);
+            }
+        }
+    }
+}
